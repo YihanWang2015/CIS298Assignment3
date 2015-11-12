209b7f789f167e1b2bd3a6b4f69966a9366b6d5d
YihanWang2015 <ywang4241@mail.kvcc.edu>
2015-11-12 00:22:44 -0500

    Try to add RecyclerView.....

--- app/src/main/java/edu/kvcc/cis298/cis298assignment3/WineListFragment.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment3/WineListFragment.java
@@ -1,11 +1,31 @@
 package edu.kvcc.cis298.cis298assignment3;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
 
 /**
  * Created by bigy on 11/11/15.
  */
 public class WineListFragment extends Fragment{
 
-    //nothing yet
+    private RecyclerView mWineRecyclerView;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_wine_list, container, false);
+
+        mWineRecyclerView = (RecyclerView) view.findViewById(R.id.wine_recycler_view);
+        mWineRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 }
